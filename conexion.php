dae98e38aa96a11d594e16e80e2fd6d338309436
YEISON <yefer08@gmail.com>
2022-11-09 23:10:55 -0500

    primer commit

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+$host       = 'localhost';
+$user       = 'root';
+$password   = '';
+$db         = 'facturacion';
+
+$conection  = @mysqli_connect($host, $user,$password,$db);
+
+if(!$conection) {
+    echo "Error de Conexión";
+}
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Login</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <section id="container">
+
+            <form action="" method="">
+                <h3> INICIAR SESIÓN</h3>
+                <img src="img/login.png" alt="login">
+
+                <input type="text" name="usuario" value="" placeholder="Usuario">
+                <input type="password" name="clave" value="" placeholder="Contraseña">
+                <p class="alert">Mensaje</p>
+                <input type="submit" name="" value="INGRESAR">
+                
+            </form>
+            
+        </section>
+    
+    </body>
+</html>
